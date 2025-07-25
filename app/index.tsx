0a8917430cd53042cd321ebd54f0f179d4deecd9
Nirma Abro <NirmaAbro143@gmail.com>
2025-07-25 11:57:13 -0600

    view like an div and text in react native with button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
+import React from "react";
+import { Button } from "react-native";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
     >
-      <Text>hey this is my app</Text>
+      <Text style={{
+        fontSize: 20,
+      }}>hey this is my app</Text>
+      <Text>hi this is my first app program </Text>
+      <Button title="click me" onPress={() => {alert("hello")}}></Button>
     </View>
   );
 }
